8d4f79530a7dbe8cff3dd417c40c4373f609b476
RobinsonXing <tongxing707@gmail.com>
2024-08-16 05:34:36 +0900

    update

--- dataset.py
+++ dataset.py
@@ -7,9 +7,9 @@ import torch
 from torch.utils.data import Dataset
 
 
-class HuMobDatasetTrain(Dataset):
-
-    def __init__(self, path_arr):
+class HuMobDatasetPreTrain(Dataset):
+    """城市A作为预训练集"""
+    def __init__(self, path):
 
         # 初始化
         self.d_array = []
@@ -19,79 +19,31 @@ class HuMobDatasetTrain(Dataset):
         self.time_delta = []
         self.label_x_array = []
         self.label_y_array = []
-        # self.label_city_array = []
         self.len_array = []
         
-        # 读取四个城市的数据集
-        for i, path in enumerate(path_arr):
-            self.df[i] = pd.read_csv(path, compression='gzip')
+        # 读取城市A的数据
+        self.df = pd.read_csv(path, compression='gzip')
 
-            for _, traj in tqdm(self.df[i].groupby('uid')):
-                # 剔除待预测用户的待预测数据点
-                #（每个城市最后3000用户的61-75天，空间坐标被mask为(999,999)）
-                traj = traj[traj['x'] != 999]
-            
-                # 转换为numpy数组
-                d = traj['d'].to_numpy()
-                t = traj['t'].to_numpy()
-                input_x = copy.deepcopy(traj['x'].to_numpy())
-                input_y = copy.deepcopy(traj['y'].to_numpy())
-                time_delta = np.insert((traj['d'].to_numpy()[1:] * 48 + 
-                                        traj['t'].to_numpy()[1:]) - 
-                                        (traj['d'].to_numpy()[:-1] * 48 + 
-                                        traj['t'].to_numpy()[:-1]), 
-                                        0, 0)
-                time_delta[time_delta > 47] = 47
-                label_x = traj['x'].to_numpy()
-                label_y = traj['y'].to_numpy()
-
-                # 对每个uid分组，随机mask15天长度的序列
-                d_unique = np.unique(d)
-                if len(d_unique[(d_unique >= np.min(d_unique)) & (d_unique <= np.max(d_unique) - 14)]) == 0:
-                    continue
-                mask_d_start = np.random.choice(d_unique[(d_unique >= np.min(d_unique)) & (d_unique <= np.max(d_unique) - 14)])
-                mask_d_end = mask_d_start + 14
-                need_mask_idx = np.where((d >= mask_d_start) & (d <= mask_d_end))
-                input_x[need_mask_idx] = 201
-                input_y[need_mask_idx] = 201
-
-                # 将所有uid分组整合到一起并保存
-                self.d_array.append(d + 1)
-                self.t_array.append(t + 1)
-                self.input_x_array.append(input_x)
-                self.input_y_array.append(input_y)
-                self.time_delta_array.append(time_delta)
-                self.label_x_array.append(label_x - 1)
-                self.label_y_array.append(label_y - 1)
-                self.len_array.append(len(d))
-
-
-class HuMobDatasetTask1Train(Dataset):
-    def __init__(self, path):
-        self.df = pd.read_csv(path)
-
-        self.d_array = []
-        self.t_array = []
-        self.input_x_array = []
-        self.input_y_array = []
-        self.time_delta_array = []
-        self.label_x_array = []
-        self.label_y_array = []
-        self.len_array = []
-
-        for uid, traj in tqdm(self.df.groupby('uid')):
-            if uid >= 80000:
-                traj = traj[traj['d'] < 60]
-
+        for _, traj in tqdm(self.df.groupby('uid')):
+            # 剔除待预测用户的待预测数据点
+            #（每个城市最后3000用户的61-75天，空间坐标被mask为(999,999)）
+            traj = traj[traj['x'] != 999]
+        
+            # 转换为numpy数组
             d = traj['d'].to_numpy()
             t = traj['t'].to_numpy()
             input_x = copy.deepcopy(traj['x'].to_numpy())
             input_y = copy.deepcopy(traj['y'].to_numpy())
-            time_delta = np.insert((traj['d'].to_numpy()[1:] * 48 + traj['t'].to_numpy()[1:]) - (traj['d'].to_numpy()[:-1] * 48 + traj['t'].to_numpy()[:-1]), 0, 0)
+            time_delta = np.insert((traj['d'].to_numpy()[1:] * 48 + 
+                                    traj['t'].to_numpy()[1:]) - 
+                                    (traj['d'].to_numpy()[:-1] * 48 + 
+                                    traj['t'].to_numpy()[:-1]), 
+                                    0, 0)
             time_delta[time_delta > 47] = 47
             label_x = traj['x'].to_numpy()
             label_y = traj['y'].to_numpy()
 
+            # 对每个uid分组，随机mask15天长度的序列
             d_unique = np.unique(d)
             if len(d_unique[(d_unique >= np.min(d_unique)) & (d_unique <= np.max(d_unique) - 14)]) == 0:
                 continue
@@ -101,6 +53,7 @@ class HuMobDatasetTask1Train(Dataset):
             input_x[need_mask_idx] = 201
             input_y[need_mask_idx] = 201
 
+            # 将所有uid分组整合到一起并保存
             self.d_array.append(d + 1)
             self.t_array.append(t + 1)
             self.input_x_array.append(input_x)
@@ -109,12 +62,12 @@ class HuMobDatasetTask1Train(Dataset):
             self.label_x_array.append(label_x - 1)
             self.label_y_array.append(label_y - 1)
             self.len_array.append(len(d))
-
+        
         self.len_array = np.array(self.len_array, dtype=np.int64)
 
     def __len__(self):
         return len(self.d_array)
-
+    
     def __getitem__(self, index):
         d = torch.tensor(self.d_array[index])
         t = torch.tensor(self.t_array[index])
@@ -124,7 +77,6 @@ class HuMobDatasetTask1Train(Dataset):
         label_x = torch.tensor(self.label_x_array[index])
         label_y = torch.tensor(self.label_y_array[index])
         len = torch.tensor(self.len_array[index])
-
         return {
             'd': d,
             't': t,
@@ -137,10 +89,11 @@ class HuMobDatasetTask1Train(Dataset):
         }
 
 
-class HuMobDatasetTask1Val(Dataset):
+class humobDatasetFineTune(Dataset):
+    """城市BCD剔除待预测点后作为微调训练集"""
     def __init__(self, path):
-        self.df = pd.read_csv(path)
 
+        # 初始化
         self.d_array = []
         self.t_array = []
         self.input_x_array = []
@@ -150,63 +103,23 @@ class HuMobDatasetTask1Val(Dataset):
         self.label_y_array = []
         self.len_array = []
 
-        self.df = self.df[self.df['uid'] >= 80000]
-        for uid, traj in tqdm(self.df.groupby('uid')):
-            d = traj['d'].to_numpy()
-            t = traj['t'].to_numpy()
-            input_x = copy.deepcopy(traj['x'].to_numpy())
-            input_y = copy.deepcopy(traj['y'].to_numpy())
-            time_delta = np.insert((traj['d'].to_numpy()[1:] * 48 + traj['t'].to_numpy()[1:]) - (traj['d'].to_numpy()[:-1] * 48 + traj['t'].to_numpy()[:-1]), 0, 0)
-            time_delta[time_delta > 47] = 47
-            label_x = traj['x'].to_numpy()
-            label_y = traj['y'].to_numpy()
+        # 读取数据集
+        self.df = pd.read_csv(path, compression='gzip')
+        # 剔除待预测用户的待预测数据点
+        self.df = self.df[self.df['x'] != 999]
 
-            mask_d_start = 60
-            mask_d_end = 74
-            need_mask_idx = np.where((d >= mask_d_start) & (d <= mask_d_end))
-            input_x[need_mask_idx] = 201
-            input_y[need_mask_idx] = 201
-
-            self.d_array.append(d + 1)
-            self.t_array.append(t + 1)
-            self.input_x_array.append(input_x)
-            self.input_y_array.append(input_y)
-            self.time_delta_array.append(time_delta)
-            self.label_x_array.append(label_x - 1)
-            self.label_y_array.append(label_y - 1)
-            self.len_array.append(len(d))
-
-        self.len_array = np.array(self.len_array, dtype=np.int64)
-
-    def __len__(self):
-        return len(self.d_array)
-
-    def __getitem__(self, index):
-        d = torch.tensor(self.d_array[index])
-        t = torch.tensor(self.t_array[index])
-        input_x = torch.tensor(self.input_x_array[index])
-        input_y = torch.tensor(self.input_y_array[index])
-        time_delta = torch.tensor(self.time_delta_array[index])
-        label_x = torch.tensor(self.label_x_array[index])
-        label_y = torch.tensor(self.label_y_array[index])
-        len = torch.tensor(self.len_array[index])
-
-        return {
-            'd': d,
-            't': t,
-            'input_x': input_x,
-            'input_y': input_y,
-            'time_delta': time_delta,
-            'label_x': label_x,
-            'label_y': label_y,
-            'len': len
-        }
+        for _, traj in tqdm(self.df.groupby('uid')):
 
 
-class HuMobDatasetTask2Train(Dataset):
+
+
+
+
+class HumobDatasetVal(Dataset):
+    """城市BCD的待预测用户作为测试集"""
     def __init__(self, path):
-        self.df = pd.read_csv(path)
-
+        
+        # 初始化
         self.d_array = []
         self.t_array = []
         self.input_x_array = []
@@ -216,125 +129,11 @@ class HuMobDatasetTask2Train(Dataset):
         self.label_y_array = []
         self.len_array = []
 
-        for uid, traj in tqdm(self.df.groupby('uid')):
-            if uid >= 22500:
-                traj = traj[traj['d'] < 60]
+        # 读取数据集
+        self.df = pd.read_csv(path, compression='gzip')
 
-            d = traj['d'].to_numpy()
-            t = traj['t'].to_numpy()
-            input_x = copy.deepcopy(traj['x'].to_numpy())
-            input_y = copy.deepcopy(traj['y'].to_numpy())
-            time_delta = np.insert((traj['d'].to_numpy()[1:] * 48 + traj['t'].to_numpy()[1:]) - (traj['d'].to_numpy()[:-1] * 48 + traj['t'].to_numpy()[:-1]), 0, 0)
-            time_delta[time_delta > 47] = 47
-            label_x = traj['x'].to_numpy()
-            label_y = traj['y'].to_numpy()
+        # 剔除掉非预测用户
+        self.df = self.df[self.df['uid'] >= len(pd.unique(self.df['uid'])) - 3000]
 
-            d_unique = np.unique(d)
-            if len(d_unique[(d_unique >= np.min(d_unique)) & (d_unique <= np.max(d_unique) - 14)]) == 0:
-                continue
-            mask_d_start = np.random.choice(d_unique[(d_unique >= np.min(d_unique)) & (d_unique <= np.max(d_unique) - 14)])
-            mask_d_end = mask_d_start + 14
-            need_mask_idx = np.where((d >= mask_d_start) & (d <= mask_d_end))
-            input_x[need_mask_idx] = 201
-            input_y[need_mask_idx] = 201
-
-            self.d_array.append(d + 1)
-            self.t_array.append(t + 1)
-            self.input_x_array.append(input_x)
-            self.input_y_array.append(input_y)
-            self.time_delta_array.append(time_delta)
-            self.label_x_array.append(label_x - 1)
-            self.label_y_array.append(label_y - 1)
-            self.len_array.append(len(d))
-
-        self.len_array = np.array(self.len_array, dtype=np.int64)
-
-    def __len__(self):
-        return len(self.d_array)
-
-    def __getitem__(self, index):
-        d = torch.tensor(self.d_array[index])
-        t = torch.tensor(self.t_array[index])
-        input_x = torch.tensor(self.input_x_array[index])
-        input_y = torch.tensor(self.input_y_array[index])
-        time_delta = torch.tensor(self.time_delta_array[index])
-        label_x = torch.tensor(self.label_x_array[index])
-        label_y = torch.tensor(self.label_y_array[index])
-        len = torch.tensor(self.len_array[index])
-
-        return {
-            'd': d,
-            't': t,
-            'input_x': input_x,
-            'input_y': input_y,
-            'time_delta': time_delta,
-            'label_x': label_x,
-            'label_y': label_y,
-            'len': len
-        }
-
-
-class HuMobDatasetTask2Val(Dataset):
-    def __init__(self, path):
-        self.df = pd.read_csv(path)
-
-        self.d_array = []
-        self.t_array = []
-        self.input_x_array = []
-        self.input_y_array = []
-        self.time_delta_array = []
-        self.label_x_array = []
-        self.label_y_array = []
-        self.len_array = []
-
-        self.df = self.df[self.df['uid'] >= 22500]
-        for uid, traj in tqdm(self.df.groupby('uid')):
-            d = traj['d'].to_numpy()
-            t = traj['t'].to_numpy()
-            input_x = copy.deepcopy(traj['x'].to_numpy())
-            input_y = copy.deepcopy(traj['y'].to_numpy())
-            time_delta = np.insert((traj['d'].to_numpy()[1:] * 48 + traj['t'].to_numpy()[1:]) - (traj['d'].to_numpy()[:-1] * 48 + traj['t'].to_numpy()[:-1]), 0, 0)
-            time_delta[time_delta > 47] = 47
-            label_x = traj['x'].to_numpy()
-            label_y = traj['y'].to_numpy()
-
-            mask_d_start = 60
-            mask_d_end = 74
-            need_mask_idx = np.where((d >= mask_d_start) & (d <= mask_d_end))
-            input_x[need_mask_idx] = 201
-            input_y[need_mask_idx] = 201
-
-            self.d_array.append(d + 1)
-            self.t_array.append(t + 1)
-            self.input_x_array.append(input_x)
-            self.input_y_array.append(input_y)
-            self.time_delta_array.append(time_delta)
-            self.label_x_array.append(label_x - 1)
-            self.label_y_array.append(label_y - 1)
-            self.len_array.append(len(d))
-
-        self.len_array = np.array(self.len_array, dtype=np.int64)
-
-    def __len__(self):
-        return len(self.d_array)
-
-    def __getitem__(self, index):
-        d = torch.tensor(self.d_array[index])
-        t = torch.tensor(self.t_array[index])
-        input_x = torch.tensor(self.input_x_array[index])
-        input_y = torch.tensor(self.input_y_array[index])
-        time_delta = torch.tensor(self.time_delta_array[index])
-        label_x = torch.tensor(self.label_x_array[index])
-        label_y = torch.tensor(self.label_y_array[index])
-        len = torch.tensor(self.len_array[index])
-
-        return {
-            'd': d,
-            't': t,
-            'input_x': input_x,
-            'input_y': input_y,
-            'time_delta': time_delta,
-            'label_x': label_x,
-            'label_y': label_y,
-            'len': len
-        }
+        
+    
