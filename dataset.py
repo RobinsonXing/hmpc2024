cfdbc25424a48b22d8a9d55104f0aec6b5d6fe0a
RobinsonXing <tongxing707@gmail.com>
2024-08-16 01:48:40 +0900

    update

--- dataset.py
+++ dataset.py
@@ -7,6 +7,65 @@ import torch
 from torch.utils.data import Dataset
 
 
+class HuMobDatasetTrain(Dataset):
+
+    def __init__(self, path_arr):
+
+        # 初始化
+        self.d_array = []
+        self.t_array = []
+        self.input_x_array = []
+        self.input_y_array = []
+        self.time_delta = []
+        self.label_x_array = []
+        self.label_y_array = []
+        # self.label_city_array = []
+        self.len_array = []
+        
+        # 读取四个城市的数据集
+        for i, path in enumerate(path_arr):
+            self.df[i] = pd.read_csv(path, compression='gzip')
+
+            for _, traj in tqdm(self.df[i].groupby('uid')):
+                # 剔除待预测用户的待预测数据点
+                #（每个城市最后3000用户的61-75天，空间坐标被mask为(999,999)）
+                traj = traj[traj['x'] != 999]
+            
+                # 转换为numpy数组
+                d = traj['d'].to_numpy()
+                t = traj['t'].to_numpy()
+                input_x = copy.deepcopy(traj['x'].to_numpy())
+                input_y = copy.deepcopy(traj['y'].to_numpy())
+                time_delta = np.insert((traj['d'].to_numpy()[1:] * 48 + 
+                                        traj['t'].to_numpy()[1:]) - 
+                                        (traj['d'].to_numpy()[:-1] * 48 + 
+                                        traj['t'].to_numpy()[:-1]), 
+                                        0, 0)
+                time_delta[time_delta > 47] = 47
+                label_x = traj['x'].to_numpy()
+                label_y = traj['y'].to_numpy()
+
+                # 对每个uid分组，随机mask15天长度的序列
+                d_unique = np.unique(d)
+                if len(d_unique[(d_unique >= np.min(d_unique)) & (d_unique <= np.max(d_unique) - 14)]) == 0:
+                    continue
+                mask_d_start = np.random.choice(d_unique[(d_unique >= np.min(d_unique)) & (d_unique <= np.max(d_unique) - 14)])
+                mask_d_end = mask_d_start + 14
+                need_mask_idx = np.where((d >= mask_d_start) & (d <= mask_d_end))
+                input_x[need_mask_idx] = 201
+                input_y[need_mask_idx] = 201
+
+                # 将所有uid分组整合到一起并保存
+                self.d_array.append(d + 1)
+                self.t_array.append(t + 1)
+                self.input_x_array.append(input_x)
+                self.input_y_array.append(input_y)
+                self.time_delta_array.append(time_delta)
+                self.label_x_array.append(label_x - 1)
+                self.label_y_array.append(label_y - 1)
+                self.len_array.append(len(d))
+
+
 class HuMobDatasetTask1Train(Dataset):
     def __init__(self, path):
         self.df = pd.read_csv(path)
