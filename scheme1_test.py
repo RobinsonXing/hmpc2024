10cd42e7c3ef7696d73094fbf26531c3245d75c1
RobinsonXing <tongxing707@gmail.com>
2024-08-19 20:39:37 +0900

    update

--- scheme1_test.py
+++ scheme1_test.py
@@ -10,7 +10,6 @@ from dataset import *
 from model import *
 
 path_arr = [
-    './dataset/cityA_groundtruthdata.csv.gz',
     './dataset/cityB_challengedata.csv.gz',
     './dataset/cityC_challengedata.csv.gz',
     './dataset/cityD_challengedata.csv.gz'
