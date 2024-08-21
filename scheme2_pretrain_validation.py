3e0a03b7eca6d60ef5fe17badf888b8744ecbfe4
RobinsonXing <tongxing707@gmail.com>
2024-08-21 09:34:24 +0900

    update

--- scheme2_pretrain_validation.py
+++ scheme2_pretrain_validation.py
@@ -23,7 +23,7 @@ def Validation(args):
     os.makedirs(result_path, exist_ok=True)
 
     # 加载验证集
-    dataset_val = ValidationSet(path_arr)
+    dataset_val = ValidationSet(path_arr[0])
     dataloader_val = DataLoader(dataset_val, batch_size=1, num_workers=args.num_workers)
 
     # 通过cuda:<device_id>指定使用的GPU
@@ -92,7 +92,7 @@ def Validation(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--pth_file', type=str, default='./checkpoint/scheme1/batchsize64_epochs100_embedsize128_layersnum4_headsnum8_cuda0_lr2e-05_seed0/2024_08_17_16_41_16.pth')
+    parser.add_argument('--pth_file', type=str, default='')     # 改为训练完成的模型的存储地址
     parser.add_argument('--num_workers', type=int, default=2)
     parser.add_argument('--embed_size', type=int, default=128)
     parser.add_argument('--layers_num', type=int, default=4)
