6e8702f35598b01f817b90f08d171c34aea98191
RobinsonXing <tongxing707@gmail.com>
2024-09-08 09:41:27 +0900

    update

--- finetune.py
+++ finetune.py
@@ -70,16 +70,16 @@ def finetune(args):
 
     # 设置日志文件名
     # name = f'finetune_batchsize{args.batch_size}_epochs{args.epochs}_embedsize{args.embed_size}_layersnum{args.layers_num}_headsnum{args.heads_num}_cuda{args.cuda}_lr{args.lr}_seed{args.seed}'
-    name = 'LPBERT-pretrainA-finetuneB'
+    name = 'LPBERT-pretrainA-finetuneC'
     current_time = datetime.datetime.now()
 
     # 初始化 wandb
-    wandb.init(project="LPBERT", name="pretrainA-finetuneB", config=args)
+    wandb.init(project="LPBERT", name="pretrainA-finetuneC", config=args)
     wandb.run.name = name  # Set the run name
     wandb.run.save()
 
     # 加载训练集
-    dataset_train = TrainSet(path_arr[1])
+    dataset_train = TrainSet(path_arr[2])
     dataloader_train = DataLoader(dataset_train, batch_size=args.batch_size, shuffle=True, collate_fn=collate_fn, num_workers=args.num_workers)
 
     # 通过cuda:<device_id>指定使用的GPU
@@ -162,7 +162,7 @@ if __name__ == '__main__':
     parser.add_argument('--embed_size', type=int, default=128)
     parser.add_argument('--layers_num', type=int, default=4)
     parser.add_argument('--heads_num', type=int, default=8)
-    parser.add_argument('--cuda', type=int, default=1)
+    parser.add_argument('--cuda', type=int, default=2)
     parser.add_argument('--lr', type=float, default=2e-5)
     parser.add_argument('--seed', type=int, default=3704)
     args = parser.parse_args()
--- pretrain.py
+++ pretrain.py
@@ -73,16 +73,16 @@ def train(args):
 
     # 设置日志文件名
     # name = f'batchsize{args.batch_size}_epochs{args.epochs}_embedsize{args.embed_size}_layersnum{args.layers_num}_headsnum{args.heads_num}_cuda{args.cuda}_lr{args.lr}_seed{args.seed}'
-    name = 'LPBERT-pretrain-cityB'
+    name = 'LPBERT-pretrain-cityAC'
     # current_time = datetime.datetime.now()
 
     # 初始化 wandb
-    wandb.init(project="LPBERT", name="pretrain_cityB", config=args)
+    wandb.init(project="LPBERT", name="pretrain_cityAC", config=args)
     wandb.run.name = name  # Set the run name
     wandb.run.save()
 
     # 加载训练集
-    dataset_train = TrainSet(path_arr[1])
+    dataset_train = TrainSet(path_arr[0:2:2])
     dataloader_train = DataLoader(dataset_train, batch_size=args.batch_size, shuffle=True, collate_fn=collate_fn, num_workers=args.num_workers)
 
     # 通过cuda:<device_id>指定使用的GPU
