3a604d854c8486f4ff24eb6ef006c00272e8b61a
RobinsonXing <tongxing707@gmail.com>
2024-09-18 19:02:57 +0900

    update

--- validation.py
+++ validation.py
@@ -92,7 +92,7 @@ def Validation(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--pth_file', type=str, default='./wandb/run-20240913_035413-mf7p34j1/files/model_2024_09_18_00_35_11_epoch79.pth')     # 改为训练完成的模型的存储地址
+    parser.add_argument('--pth_file', type=str, default='./wandb/run-20240913_035413-mf7p34j1/files/model_2024_09_18_16_35_40_epoch90.pth')     # 改为训练完成的模型的存储地址
     parser.add_argument('--num_workers', type=int, default=2)
     parser.add_argument('--embed_size', type=int, default=128)
     parser.add_argument('--city_embed', type=int, default=4)
